4b9d53538c220fca7c0762e3b4926c612b316f64
PowerOleg <geraskin.oleg@mail.ru>
2025-03-29 17:37:50 +0300

    openssl

--- search/crawler.h
+++ search/crawler.h
@@ -5,6 +5,7 @@
 #include <boost/beast/version.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/beast/websocket.hpp>
 #include <cstdlib>
 #include "common.h"
 
@@ -12,17 +13,19 @@ namespace beast = boost::beast;     // from <boost/beast.hpp>
 namespace http = beast::http;       // from <boost/beast/http.hpp>
 namespace net = boost::asio;        // from <boost/asio.hpp>
 using tcp = net::ip::tcp;           // from <boost/asio/ip/tcp.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
 
 class Crawler
 {
 public:
-	Crawler(const std::string host_, const std::string target_);
-	int HttpRequest();
+	Crawler(std::string host_, const std::string target_);
+	int SimpleHttpRequest();
+	int HttpWebSocketRequest();
 
 private:
-	const std::string host;
+	std::string host;
     const std::string target;
-	const std::string port;
+	const std::string port = "80";
 	const int version = 11;
 };
 
--- search/main.cpp
+++ search/main.cpp
@@ -1,6 +1,8 @@
-﻿#include "file_manager.h"
-#include "crawler.h"
-#include "postgres_manager.h"
+﻿
+//#include "crawler.h"
+//#include "postgres_manager.h"
+#include "webloader.h"
+#include "file_manager.h"
 
 
 struct Config
@@ -35,15 +37,77 @@ int main(int argc, char** argv)
 		&config.crowler_depth,
 		&config.http_port);
 
-	Postgres_manager postgres("localhost", "5432", "dvdrental", "postgres", "106");
-	//postgres.Test();
 
-
-	std::string host = "httpbin.org";
-	std::string target = "/get";
+	//std::string host = "httpbin.org";
+	//std::string target = "/get";
+	
+	//std::string host = "github.com";
+	//std::string target = "/PowerOleg?tab=repositories";
+	//https://mail.ru/
+	std::string host = "mail.ru";
+	std::string target = "/";
+	 
 	//crowler.Mapping(config.url);
-	Crawler crawler(host, target);
-	//crawler.HttpRequest();
+	//Crawler crawler(host, target);
+	//crawler.HttpWebSocketRequest();
 
+
+	//Postgres_manager postgres("localhost", "5432", "dvdrental", "postgres", "106");
+	//postgres.Test();
 	return 0;
 }
+
+// протестировано в MSVS2017
+//int main2()
+//{
+//    system("chcp 1251");
+//
+//    boost::asio::io_context ioc;
+//    Webloader ldr{ ioc };
+//
+//    std::vector<std::string> vUri
+//    {
+//        "https://3dnews.ru/" // начальная ссылка
+//    };
+//
+//    std::unordered_set<std::string> ustUsed{ vUri.begin(), vUri.end() }; // для отработанных ссылок
+//
+//    std::vector<std::vector<std::string>> vres; // найденные ссылки
+//
+//    int dpth = 2; // глубина обхода
+//    while (dpth--)
+//    {
+//        vres.clear();
+//        vres.reserve(vUri.size()); // чтобы не переаллоцировался
+//
+//        // вектор vUri обрабатывается пулом из 4 потоков
+//        boost::asio::thread_pool tpool{ 4 };
+//        for (auto const& sUri : vUri)
+//        {
+//            vres.emplace_back();
+//            boost::asio::post(tpool, std::bind(&Loader::load, &ldr, std::cref(sUri), std::ref(vres.back())));
+//        }
+//        tpool.join();
+//
+//        // вывод результата
+//        removeDuplicatesFragments(vres, ustUsed);
+//        std::cout << "\n*********************************** Список из " << vUri.size() << " просмотренных страниц: ****************************************\n\n";
+//        for (int i = 0; i < vres.size(); ++i)
+//        {
+//            std::cout << "Ссылок: " << std::setw(5) << std::left << vres.at(i).size() << " для страницы: " << vUri.at(i) << "\n\n";
+//            for (auto const& str : vres.at(i))
+//            {
+//                //std::cout << str << std::endl; // вывод найденных ссылок
+//            }
+//        }
+//
+//        // перед заходом на следующую итерацию перекидываем все найденные ссылки из vres в vUri 
+//        absLinks(vUri, vres);
+//        vUri.clear();
+//        for (auto const& vLnk : vres)
+//        {
+//            vUri.insert(vUri.end(), vLnk.begin(), vLnk.end());
+//        }
+//    }
+//
+//}

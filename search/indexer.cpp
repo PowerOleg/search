603ce62971b8623e51b8a1330d57f1711b929a7d
PowerOleg <geraskin.oleg@mail.ru>
2025-03-30 18:36:33 +0300

    indexer gumbo extract_text()

--- search/indexer.cpp
+++ search/indexer.cpp
@@ -1,13 +1,75 @@
 #include "indexer.h"
 
-Indexer::Indexer(std::string text)
+Indexer::Indexer(const std::string page_body)
 {
-	this->page_body = text;
+	this->page_body = page_body;
 	printf("Indexing...");
+    std::vector<std::string> hrefs;
+
+
+
+    GumboAttribute* hrefBase = nullptr;
+    GumboOutput* output = gumbo_parse(page_body.c_str());
+
+    std::queue<GumboNode*> qn;
+    qn.push(output->root);
+
+    while (!qn.empty())
+    {
+        GumboNode* node = qn.front();
+        qn.pop();
+        if (GUMBO_NODE_ELEMENT == node->type)
+        {
+            ExtractText(node);
+
+            /*GumboNode* title_text = static_cast<GumboNode*>(node->v.element.children.data[0]);
+            if (title_text->type == GUMBO_NODE_TEXT)
+            {
+                auto text1 = title_text->v.text.text;
+                std::cout << text1 << std::endl;
+            }*/
+
+
+            //const char* text = node->v.text.text;
+            //std::cout << text << std::endl;
+
+
+            
+
+            /*GumboAttribute* href = nullptr;
+            if (node->v.element.tag == GUMBO_TAG_A && (href = gumbo_get_attribute(&node->v.element.attributes, "href")))
+            {
+                std::string sLnk{ href->value };
+                if (!sLnk.empty())
+                {
+                    hrefs.emplace_back(href->value);
+                }
+            }
+            else if (node->v.element.tag == GUMBO_TAG_BASE && (hrefBase = gumbo_get_attribute(&node->v.element.attributes, "href")))
+            {
+            }
+            GumboVector* children = &node->v.element.children;
+            for (unsigned int i = 0; i < children->length; ++i)
+            {
+                qn.push(static_cast<GumboNode*>(children->data[i]));
+            }*/
+            
+        }
+        
+    }
+    
 }
 
-std::vector<std::string> Indexer::getWords()
-{
-	std::vector<std::string> words;
-	return words;
+void Indexer::ExtractText(GumboNode* node) {
+   // [4] (https://best-of-web.builder.io/library/google/gumbo-parser)
+    if (node->type == GUMBO_NODE_TEXT) {
+        printf("Text: %s\n", node->v.text.text);// [4] (https://best-of-web.builder.io/library/google/gumbo-parser)
+    }
+    else if (node->type == GUMBO_NODE_ELEMENT)
+    {
+        GumboVector* children = &node->v.element.children;
+        for (unsigned int i = 0; i < children->length; ++i) {
+            ExtractText((GumboNode*)children->data[i]);
+        }
+    }
 }
--- search/indexer.h
+++ search/indexer.h
@@ -1,13 +1,27 @@
 #pragma once
 #include <string>
+#include <iostream>
 #include <vector>
+#include <queue>
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+//#include <gumbo.h>
+#include "gumbo.h"
 
 class Indexer
 {
 public:
-	Indexer(std::string text);
-	std::vector<std::string> getWords();
+	Indexer(const std::string text);
+	std::vector<std::string> getWords() { return this->words; }
+	void ExtractText(GumboNode* node);
 private:
 	std::string page_body;
+	std::vector<std::string> words;
 };
 
